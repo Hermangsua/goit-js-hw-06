9c80ca2724e87aaa75237b6d4fd746bf8e7cb8c4
Hermangsua <hermangsua@gmail.com>
2022-12-09 00:10:03 +0200

    upd
    last save

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,5 @@
 const textInput = document.querySelector("#validation-input");
 textInput.addEventListener("blur", () => {
-  console.log(textInput.getAttribute("data-length"));
-  console.log(textInput.value.length);
   if (textInput.getAttribute("data-length") == textInput.value.length) {
     textInput.classList.remove("invalid");
     textInput.classList.add("valid");
